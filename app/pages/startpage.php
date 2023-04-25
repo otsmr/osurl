9cf676e7ffa1e828044d468809a7b41bd1d74927
otsmr <git@tsmr.eu>
2023-04-25 21:31:18 +0200

    ads

--- app/pages/startpage.php
+++ app/pages/startpage.php
@@ -1,7 +1,7 @@
 <main>
     <header>
         <h1>Kurz-URL-Dienst</h1>
-        <p class='desc'>Ein Service von <a href='https://oproj.de'>oproj.de.</a></p>
+        <p class='desc'>Ein Service von <a href='https://tsmr.eu'>tsmr.eu.</a></p>
     </header>
     <a href="<?php echo $odmin->get_signin_url(); ?>" ><button class="button center">Anmelden</button><a>
     <?php include __DIR__ . "/../includes/footer.php" ?>
